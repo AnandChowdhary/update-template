dab5d34e0a75d63be5daedad501a312e0e9433e0
Anand Chowdhary <mail@anandchowdhary.com>
2020-08-12 14:56:20 +0530

    :heavy_plus_sign: Add fast-glob

--- update.ts
+++ update.ts
@@ -1,11 +1,15 @@
 import { execSync } from "child_process";
+import recursiveReaddir from "recursive-readdir";
+import { join } from "path";
 
 export const update = async () => {
   console.log("Updating...");
   const repoUrl = process.argv[2];
   if (!repoUrl) throw new Error("Provide a repository URL");
-  const tempDir = "tempDir";
+  const tempDir = `tempDir_${Math.random().toString(32)}`;
   execSync(`git clone ${repoUrl} ${tempDir}`);
+  const files = await recursiveReaddir(join(".", tempDir));
+  console.log(files);
 };
 
 update();
