0412b3406ca29586281dd4e113aac8f819bb4954
Anand Chowdhary <mail@anandchowdhary.com>
2020-08-12 13:59:15 +0530

    :construction: Clone repo

--- update.ts
+++ update.ts
@@ -1,5 +1,11 @@
+import { execSync } from "child_process";
+
 export const update = async () => {
   console.log("Updating...");
+  const repoUrl = process.argv[2];
+  if (!repoUrl) throw new Error("Provide a repository URL");
+  const tempDir = "tempDir";
+  execSync(`git clone ${repoUrl} ${tempDir}`);
 };
 
 update();
